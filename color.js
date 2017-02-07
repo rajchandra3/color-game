8c4b015f3d76263e175e6a9056014dd886dd1439
devil-Coder <raj.chandra2016@vitstudent.ac.in>
2017-02-07 08:53:30 +0530

    Update color.js

--- color.js
+++ color.js
@@ -36,7 +36,7 @@ for(var i=0 ; i<6; i++)
         else
         {
             isCorrect.textContent="Opps! That is wrong...";
-            this.style.background="rgb(,,)";
+            this.style.background="white";
         }
         });
 }

8a12dc1c0ad286a69fe664ef764ddeb19414f5ce
rajchandra3 <rajchandra963@gmail.com>
2020-03-28 17:44:58 +0530

    add ads for mobile view

---
+++ src/js/ads.js
@@ -0,0 +1,33 @@
+const h_ads=document.querySelectorAll('.horizontal-ads');
+for(ad of h_ads){
+    ad.innerHTML=`
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- color-tile-horizontal-ads -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-8598751574376549"
+        data-ad-slot="5996761391"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    `
+}
+const v_ads=document.querySelectorAll('.vertical-ads');
+for(ad of v_ads){
+    ad.innerHTML=`
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- color-tile-ads -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-8598751574376549"
+        data-ad-slot="6993669367"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    `
+}
+

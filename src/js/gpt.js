47c22da086b0c5dde467be90fb33bbd6a3f0c6d4
rajchandra3 <rajchandra963@gmail.com>
2020-09-30 15:35:18 +0530

    add gpt ad for testing

---
+++ src/js/gpt.js
@@ -0,0 +1,26 @@
+// window.googletag = window.googletag || {cmd: []};
+// googletag.cmd.push(function() {
+// googletag
+//     .defineSlot(
+//         '/6993669367/Travel/Europe/France/Paris', [300, 250], 'gpt-ad-large')
+//     .addService(googletag.pubads());
+
+//     const mapping = googletag.sizeMapping()
+//     .addSize([1024, 768], [[750, 200], [728, 90]])
+//     .addSize([640, 480], [300, 250])
+//     .addSize([0, 0], [])
+//     .build();
+
+//     adSlot.defineSizeMapping(mapping);
+//     googletag.enableServices();
+// });
+
+
+window.googletag = window.googletag || {cmd: []};
+googletag.cmd.push(function() {
+  googletag
+      .defineSlot(
+          '/6993669367/Travel/Europe/Paris', [300, 250], 'banner-ad')
+      .addService(googletag.pubads());
+  googletag.enableServices();
+});
